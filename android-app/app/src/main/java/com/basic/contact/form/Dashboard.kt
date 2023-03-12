1142b6282b7b106a7704586ff3bed7919df60559
etnms <etiennenemes@gmail.com>
2023-03-12 19:22:59 -0400

    added basic read logic from firestore in android

--- android-app/app/src/main/java/com/basic/contact/form/Dashboard.kt
+++ android-app/app/src/main/java/com/basic/contact/form/Dashboard.kt
@@ -1,16 +1,20 @@
 package com.basic.contact.form
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.marginTop
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-
+import org.w3c.dom.Text
 
 class Dashboard : AppCompatActivity() {
 
@@ -37,10 +41,29 @@ class Dashboard : AppCompatActivity() {
     }
 
     private fun readData() {
+        val linearLayout = findViewById<LinearLayout>(R.id.items)
         db.collection("messages")
             .get()
             .addOnSuccessListener { result ->
                 for (document in result) {
+                    val frameLayout = FrameLayout(this)
+
+                    val contactText = TextView(this)
+                    val messageText = TextView(this)
+                    val dateText = TextView(this)
+
+                    contactText.text = document.data[("contact")].toString()
+                    messageText.text = document.data[("message")].toString()
+                    dateText.text = document.data[("date")].toString()
+
+                    //frameLayout.addView(contactText)
+                    //frameLayout.addView(messageText)
+                    //frameLayout.addView(dateText)
+
+                    linearLayout.addView(contactText)
+                    linearLayout.addView((messageText))
+                    linearLayout.addView((dateText))
+
                     Log.d("data", "${document.id} => ${document.data}")
                 }
             }
