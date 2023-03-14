10ec710f20a22eec6f1df9d384f14eb617ce3727
etnms <etiennenemes@gmail.com>
2023-03-14 13:31:17 -0400

    added basic style for list on android

--- android-app/app/src/main/java/com/basic/contact/form/Dashboard.kt
+++ android-app/app/src/main/java/com/basic/contact/form/Dashboard.kt
@@ -5,16 +5,21 @@ import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.core.view.marginTop
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import org.w3c.dom.Text
+import java.text.SimpleDateFormat
+import java.util.*
 
 class Dashboard : AppCompatActivity() {
 
@@ -46,7 +51,12 @@ class Dashboard : AppCompatActivity() {
             .get()
             .addOnSuccessListener { result ->
                 for (document in result) {
-                    val frameLayout = FrameLayout(this)
+                    val cardView = CardView(this)
+                    cardView.setBackgroundColor(ContextCompat.getColor(this,  R.color.purple_200))
+
+
+                    val layoutItems = LinearLayout(this)
+                    layoutItems.orientation = LinearLayout.VERTICAL
 
                     val contactText = TextView(this)
                     val messageText = TextView(this)
@@ -54,17 +64,25 @@ class Dashboard : AppCompatActivity() {
 
                     contactText.text = document.data[("contact")].toString()
                     messageText.text = document.data[("message")].toString()
-                    dateText.text = document.data[("date")].toString()
+                    val timestamp = document.data[("date")] as com.google.firebase.Timestamp
+                    val milliseconds = timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000
+                    val sdf = SimpleDateFormat("dd/MM/yyyy hh:mm:ss")
+                    val netDate = Date(milliseconds)
+                    val date = sdf.format(netDate).toString()
+                    dateText.text = date
 
-                    //frameLayout.addView(contactText)
-                    //frameLayout.addView(messageText)
-                    //frameLayout.addView(dateText)
+                    layoutItems.addView(contactText)
+                    layoutItems.addView(messageText)
+                    layoutItems.addView(dateText)
 
-                    linearLayout.addView(contactText)
-                    linearLayout.addView((messageText))
-                    linearLayout.addView((dateText))
+                    cardView.addView((layoutItems))
+                    linearLayout.addView(cardView)
+                    val marginLayoutParams = cardView.layoutParams as ViewGroup.MarginLayoutParams
+                    marginLayoutParams.setMargins(30, 16, 30, 16)
 
-                    Log.d("data", "${document.id} => ${document.data}")
+
+                    cardView.layoutParams = marginLayoutParams
+
                 }
             }
             .addOnFailureListener { exception ->
